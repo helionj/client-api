3fa169e1ca624d0716ffc617ddf1c85c9b699437
helionporto <helionporto@hotmail.com>
2021-05-16 15:24:01 -0400

    Client findById and exceptions

--- src/main/java/com/helion/clientapi/services/ClientService.java
+++ src/main/java/com/helion/clientapi/services/ClientService.java
@@ -1,13 +1,17 @@
 package com.helion.clientapi.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.helion.clientapi.dtos.ClientDTO;
 import com.helion.clientapi.entities.Client;
 import com.helion.clientapi.repositories.ClientRepository;
+import com.helion.clientapi.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -15,9 +19,17 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
+	@Transactional(readOnly = true)
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 		Page<Client> list = repository.findAll(pageRequest);
 		return list.map(x -> new ClientDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id){
+		Optional<Client> obj  = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
+		return new ClientDTO(entity);
 	}
 
 	
