d120d95c9b4715b7a764f3f579eab746b1886251
helionporto <helionporto@hotmail.com>
2021-05-16 16:44:07 -0400

    Client update and delete

--- src/main/java/com/helion/clientapi/services/ClientService.java
+++ src/main/java/com/helion/clientapi/services/ClientService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.helion.clientapi.dtos.ClientDTO;
 import com.helion.clientapi.entities.Client;
 import com.helion.clientapi.repositories.ClientRepository;
+import com.helion.clientapi.services.exceptions.DatabaseException;
 import com.helion.clientapi.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -64,6 +67,19 @@ public class ClientService {
 		entity.setIncome(dto.getIncome());
 		entity.setBirthDate(dto.getBirthDate());
 		entity.setChildren(dto.getChildren());
+		
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e){
+			throw new ResourceNotFoundException("Id não encontrado na base de dados : " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Violação de integridade");
+		}
 		
 	}
 	
